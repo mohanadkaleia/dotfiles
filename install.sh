e81b5ed14b5f6d1ec9ed0e8c26acc71464ff9e66
Mohanad Kaleia <ms.kaleia@gmail.com>
2021-02-13 18:21:18 -0800

    remove useless stuff

--- install.sh
+++ install.sh
@@ -4,19 +4,6 @@
 # Bootstrap script for setting up a new OSX machine
 # 
 # This should be idempotent so it can be run multiple times.
-#
-# Notes:
-#
-# - If installing full Xcode, it's better to install that first from the app
-#   store before running the bootstrap script. Otherwise, Homebrew can't access
-#   the Xcode libraries as the agreement hasn't been accepted yet.
-#
-# Reading:
-#
-# - http://lapwinglabs.com/blog/hacker-guide-to-setting-up-your-mac
-# - https://gist.github.com/MatthewMueller/e22d9840f9ea2fee4716
-# - https://news.ycombinator.com/item?id=8402079
-# - http://notes.jerzygangi.com/the-best-pgp-tutorial-for-mac-os-x-ever/
 
 # helpers
 function echo_ok() { echo -e '\033[1;32m'"$1"'\033[0m'; }
